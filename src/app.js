925418547f5581329dbfe08766400b5b7b786098
nitin-p-simformsolutions <nitin-p-simformsolutions@users.noreply.github.com>
2021-02-01 20:07:04 +0530

    testing application with jest module

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('./db/mongoose');
+const app = express();
+
+const userRouter = require('./routers/user');
+app.use(userRouter)
+
+const taskRouter = require('./routers/tasks');
+app.use(taskRouter)
+
+
+app.use(express.json());
+
+
+module.exports = app;
