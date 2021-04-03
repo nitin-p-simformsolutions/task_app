0be91f9c14adda659106837924363e0a4046dcf0
nitin <nitinpal7069@gmail.com>
2021-04-03 13:02:03 +0530

    new file

---
+++ 01Basics/array.js
@@ -0,0 +1,75 @@
+ var array = ["nitin","nilesh","naitik"];
+//  for (i in array){
+//      console.log(array[i]);
+//  }
+
+//  for (i of array){
+//     console.log(i);
+// }
+var name ="naitik";
+for(i in name){
+    console.log(name[i]);
+}
+for (i of name){
+    console.log(i);
+}
+// // console.log(array[0]);
+// // console.log(typeof(array));
+
+// // array[0] = Date.now();
+// // console.log(array[0]);
+// //arrays properties and methods
+
+// // console.log(array.length);
+// // console.log(array.sort());
+// // for(var i=0;i<array.length;i++){
+// //     console.log(array[i]);
+// // }
+
+// array.push("yuvaan");
+// // for(var i=0;i<array.length;i++){
+// //         console.log(array[i]);
+// //     }
+
+// // console.log(Array.isArray(array));
+
+// // console.log(array.toString());
+
+// // var x = array.pop();
+// // console.log(x);
+// var y = array.push("prisha");
+// //console.log(array.length);
+// // console.log(array.unshift("pihu"));
+
+// var myGirls = ["Cecilie", "Lone"];
+// var myBoys = ["Emil", "Tobias", "Linus"];
+// var myChildren = myGirls.concat(myBoys);
+// // console.log(myChildren.length);
+// // console.log(myChildren.reverse());
+
+// //myChildren.forEach((s)=>{console.log(s);})
+
+// var numbers = [45, 4, 9, 16, 25];
+
+// var over18 = numbers.filter((value)=>{return value>18});
+// console.log(over18);
+
+// var sum = numbers.reduce((total, value)=>{return(total+value) ;})
+// console.log(sum);
+
+// var allover18 = numbers.every((value)=>{
+//     return value>18;
+// })
+// console.log(allover18);
+// var someover = numbers.some((value)=>{
+//     return value>18;
+// })
+// console.log(someover);
+
+// var fin = numbers.findIndex((value)=>{
+//     return value>18;
+// })
+// console.log(fin);
+// console.log(Math.min(numbers));
+
+//console.log(Math.floor(Math.random()*12));
---
+++ 01Basics/celcius.js
@@ -0,0 +1,29 @@
+var score = 100
+
+score  = 145
+
+const bonus = 50
+
+var finalScore = (score+bonus)*1.8
+
+var complex = ((5+5)*4)%2
+
+// console.log(finalScore)
+// console.log(complex)
+
+let tempInFahrenheit = 100
+
+let celcius = (tempInFahrenheit -32)*5/9
+//console.log(celcius)
+
+false == ""  
+false == []  
+false == {}  
+"" == 0      
+"" == []     
+"" == {}     
+0 == []      
+0 == {}      
+0 == null
+
+
